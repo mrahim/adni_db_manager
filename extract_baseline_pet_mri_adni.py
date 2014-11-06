89e75c87979645dff7939513a5eeb62dcc13f618
Mehdi Rahim <mehdi.rahim@cea.fr>
2014-11-06 18:46:48 +0100

    extracting pet baseline

--- extract_baseline_pet_mri_adni.py
+++ extract_baseline_pet_mri_adni.py
@@ -3,6 +3,7 @@
 """
 
 import os, glob
+import numpy as np
 import pandas as pd
 
 BASE_DIR = os.path.join('csv', 'fetch_results')
@@ -14,15 +15,40 @@ pet = pd.read_csv(os.path.join(BASE_DIR, 'pet_fdg_uniform_baseline.csv'))
 
 pet_mri = pd.merge(pet, mri, on='Subject_ID', how='inner')
 
-cfile_list = []
 
-for subject_id in pet_mri['Subject_ID']:
-    xml_file = glob.glob(os.path.join(ADNI_DIR,
-                                      'ADNI_' + subject_id + '*.xml'))
-    print xml_file
-    break
+# Create a directory which contains MRI and PET for all subjects
 
-
+for image_id in np.concatenate((pet_mri['Image_ID_x'].values,
+                                pet_mri['Image_ID_y'].values)):
+    xml_list = glob.glob(os.path.join(ADNI_DIR, '*I' + str(image_id) + '.xml'))
+    
+    if len(xml_list)==0:
+        continue
+    
+    xml_file = xml_list[0]
+    filename = os.path.split(xml_file)[1].split('.')[0]
+    subject_id = '_'.join(filename.split('_', 4)[1:4])
+    description = filename.split('_', 4)[4].rsplit('_', 2)[0]
+    sequence_id = filename.rsplit('_', 2)[1]
+    folderpath = os.path.join(ADNI_DIR, subject_id, description)
+    
+    for d in os.listdir(folderpath):
+        print sequence_id
+        print d, os.listdir(os.path.join(folderpath, d))
+        
+    
+    
+    """
+    for root, dirs, files in os.walk(folderpath):
+        for d in dirs:
+            #print d, image_id
+            a = re.match('^[SI]' + str(image_id), d)
+            print a, d, image_id
+            if d == 'I' + str(image_id):
+                image_folder = os.path.join(root, d)
+                break
+    #print image_folder
+"""
 
 """
 for image_id in pet_mri['Image_ID']:
