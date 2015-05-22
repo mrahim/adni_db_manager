e13ed5f69070c6c10c0797e2aab2cc54dd298888
Mehdi Rahim <mehdi.rahim@cea.fr>
2015-05-22 17:12:57 +0200

    dx

--- extract_longitudinal_dx_group.py
+++ extract_longitudinal_dx_group.py
@@ -5,6 +5,33 @@ Created on Fri May 22 15:15:01 2015
 @author: mehdi.rahim@cea.fr
 """
 
-import os
+import os, glob
 import pandas as pd
+import numpy as np
 
+csv_file = 'Diagnosis/'
+dataset_dir = '/disk4t/mehdi/data/ADNI_extracted/fmri_longitudinal/'
+
+
+roster = pd.read_csv(os.path.join(csv_file, 'ROSTER.csv'))
+dx = pd.read_csv(os.path.join(csv_file, 'DXSUM_PDXCONV_ADNIALL.csv'))
+
+
+def get_dx(rid, sid):
+    """Returns all diagnoses for a given
+    rid and sid"""
+    return dx[(dx.RID == rid) & (dx.SITEID == sid)][['EXAMDATE', 'DX_CURREN', 'DX_CHANGE']].values
+
+
+
+
+subj_list = os.listdir(dataset_dir)
+for subj_id in subj_list:
+    rid = roster[roster.PTID == subj_id]['RID'].values
+    sid = roster[roster.PTID == subj_id]['SITEID'].values
+    if len(rid) > 0 and len(sid) > 0:
+        rid = rid[0]
+        sid = sid[0]
+        print sid, rid, get_dx(rid, sid)
+    else:
+        print 'something is missing'
