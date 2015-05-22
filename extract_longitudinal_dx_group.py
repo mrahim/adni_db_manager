0f054134d5fb5cb5b70bf6999f96aad8e2793ec6
Mehdi Rahim <mehdi.rahim@cea.fr>
2015-05-22 17:25:07 +0200

    dx

--- extract_longitudinal_dx_group.py
+++ extract_longitudinal_dx_group.py
@@ -20,9 +20,23 @@ dx = pd.read_csv(os.path.join(csv_file, 'DXSUM_PDXCONV_ADNIALL.csv'))
 def get_dx(rid, sid):
     """Returns all diagnoses for a given
     rid and sid"""
-    return dx[(dx.RID == rid) & (dx.SITEID == sid)][['EXAMDATE', 'DX_CURREN', 'DX_CHANGE']].values
+    return dx[(dx.RID == rid) & (dx.SITEID == sid)][['EXAMDATE', 'DXCURREN', 'DXCHANGE']].values
 
 
+
+
+# DX_CURREN: 1=NL;2=MCI;3=AD
+# DX_CONV : 1=Yes - Conversion;2=Yes - Reversion; 0=No
+# DX_CONTYP 1=Normal Control to MCI; 2=Normal Control to AD; 3=MCI to AD
+# DX_CHANGE: 1=Stable: NL to NL; 2=Stable: MCI to MCI; 
+# 3=Stable: Dementia to Dementia; 4=Conversion: NL to MCI;
+# 5=Conversion: MCI to Dementia; 6=Conversion: NL to Dementia;
+# 7=Reversion: MCI to NL; 8=Reversion: Dementia to MCI;
+# 9=Reversion: Dementia to NL
+# DX_NORM, DX_MCI, DX_AD
+# DX_ADES (1,2,3) : 1=Mild; 2=Moderate; 3=Severe
+# DXAPP : 1=Probable; 2=Possible
+#
 
 
 subj_list = os.listdir(dataset_dir)
@@ -35,3 +49,4 @@ for subj_id in subj_list:
         print sid, rid, get_dx(rid, sid)
     else:
         print 'something is missing'
+    break
